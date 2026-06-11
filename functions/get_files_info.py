5374086a5225c41aaea6d5e7bc9947d58b7713ff
RamyGarici <ramymanilgarici@gmail.com>
2026-06-11 13:20:40 +0100

    Add: List files

---
+++ functions/get_files_info.py
@@ -0,0 +1,31 @@
+import os
+
+def get_files_info(working_directory: str, directory: str = ".") -> str:
+ try:
+    absolute_path = os.path.abspath(working_directory)
+    full_path = os.path.normpath(os.path.join(absolute_path, directory))
+    valid_target_dir = os.path.commonpath([full_path, absolute_path]) == absolute_path
+   
+
+    if not valid_target_dir:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(full_path):
+        return f'Error: "{directory}" is not a directory' 
+    
+    results = []
+    for item in os.listdir(full_path):
+       results.append(f"{item}: file_size={os.path.getsize(os.path.join(full_path,item))} bytes, is_dir={os.path.isdir(os.path.join(full_path,item))}")
+    return "\n".join(results)
+   
+ 
+
+
+
+
+ except Exception as e:
+    return f"Error: {e}"
+
+
+
+
+
